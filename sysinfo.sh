91b69de427c5ba671600a9afc94918cc0e067ae8
Balqis Nabilah <aqis.0309@gmail.com>
2026-07-03 14:54:00 +0000

    Initialize bash script with hostname and date/time collection

---
+++ sysinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "========================================"
+echo "       LINUX SYSTEM INFORMATION         "
+echo "========================================"
+
+echo ""
+echo "--- Hostname ---"
+hostname
+
+echo ""
+echo "--- Current Date and Time ---"
+date
